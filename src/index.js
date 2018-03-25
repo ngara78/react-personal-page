68f9cb18488b967be03e5255d19dbfab68066ca3
ngara <ngara78@gmail.com>
2018-03-25 18:16:02 +0200

    React added First react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello Boilerplatte!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Hello Boilerplate';
+
+ReactDOM.render(
+    <div>{title}</div>,
+    document.getElementById('app')
+);
